b906a80e9fd93846bc00232bbe4824d1e47386e7
Andressa Tonietto Reis <andressa@cargon.com>
2021-03-24 08:25:39 -0300

    add RecordsTable component

---
+++ src/components/RecordsTable/index.js
@@ -0,0 +1,27 @@
+/* eslint-disable no-unused-vars */
+import React, { useEffect, useState } from 'react';
+import api from 'services/api';
+import { useStore } from 'store';
+import RecordsTable from './RecordsTable';
+
+const RecordsTableContainer = () => {
+  const records = useStore(state => state.timeRecords);
+  const setRecords = useStore(state => state.setPlease);
+
+  const getRecords = async () => {
+    try {
+      const res = await api.get('records?_sort=id&_order=desc&_start=0&_end=7');
+      setRecords(res.data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  useEffect(() => {
+    getRecords();
+  }, []);
+
+  return <RecordsTable records={records} />;
+};
+
+export default RecordsTableContainer;
