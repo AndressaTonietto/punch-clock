2aa29b47a384b8f3fe0a4dd3e658464f9f3bca4d
Andressa Tonietto Reis <andressa@cargon.com>
2021-03-24 20:46:00 -0300

    add json server files

--- server.js
+++ server.js
@@ -4,16 +4,12 @@ const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 
-// Set default middlewares (logger, static, cors and no-cache)
 server.use(middlewares);
 
-// Add custom routes before JSON Server router
 server.get('/echo', (req, res) => {
   res.jsonp(req.query);
 });
 
-// To handle POST, PUT and PATCH you need to use a body-parser
-// You can use the one used by JSON Server
 server.use(jsonServer.bodyParser);
 server.use((req, res, next) => {
   const now = new Date();
@@ -22,11 +18,9 @@ server.use((req, res, next) => {
   } else if (req.method === 'PUT') {
     req.body.updatedAt = now;
   }
-  // Continue to JSON Server router
   next();
 });
 
-// Use default router
 server.use(router);
 server.listen(9000, () => {
   console.log('JSON Server is running');
