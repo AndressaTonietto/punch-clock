4fe3cde389a72b14ba76e6901a8620f5342c5014
Andressa Tonietto Reis <andressa@cargon.com>
2021-03-26 23:36:07 -0300

    fixed submit validation e did some styling

--- src/components/SaveRecordForm/index.js
+++ src/components/SaveRecordForm/index.js
@@ -1,31 +1,97 @@
 import React, { useState } from 'react';
-import styled from 'styled-components';
-
-import { GrDocumentTime } from 'react-icons/gr';
-
-import { calculateTimeBank, convertToDate } from 'utils';
-import { useStore } from 'store';
-import differenceInMinutes from 'date-fns/differenceInMinutes';
 
 import api from 'services/api';
+import styled from 'styled-components';
+import { GrDocumentTime } from 'react-icons/gr';
 
-const StyledForm = styled.form``;
-const StyledFormGroup = styled.div``;
-const StyledLabel = styled.label``;
-const StyledInput = styled.input``;
-const Message = styled.div`
-  margin-bottom: 0.5em;
-  color: palevioletred;
-  display: block;
+import { useStore } from 'store';
+import { calculateTimeBank, convertToDate } from 'utils';
+import differenceInMinutes from 'date-fns/differenceInMinutes';
+
+import Card from 'components/Card';
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+
+  @media (min-width: 640px) {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+  }
+
+  input,
+  button {
+    input::-webkit-outer-spin-button,
+    input::-webkit-inner-spin-button {
+      -webkit-appearance: none;
+      margin: 0;
+    }
+    background: none;
+    border: none;
+    :focus {
+      outline: none;
+    }
+    border-radius: 0px;
+    background: #a18ca1;
+    box-shadow: inset -5px 5px 9px #917e91, inset 5px -5px 9px #b19ab1;
+
+    /* Firefox */
+    input[type='number'] {
+      -moz-appearance: textfield;
+    }
+
+    padding: 10px 5px;
+    .no-outline:focus {
+      outline: none;
+    }
+    color: #00122e;
+  }
 `;
-const StyledButton = styled.button``;
 
-const Form = () => {
-  // const records = useStore(state => state.records);
+const Input = styled.input`
+  width: 85px;
+  height: 20px;
+  margin: 0 15px;
+  flex: 1;
+`;
+
+const Button = styled.button`
+  width: 40px;
+  height: 40px;
+  margin: 18px 15px 0 15px;
+  align-self: flex-end;
+`;
+
+const FormGroup = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+
+  @media (min-width: 640px) {
+    display: flex;
+    flex-direction: column;
+  }
+`;
+
+const ErrorMessage = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-evenly;
+  margin-top: 18px;
+`;
+
+const Label = styled.label`
+  flex: 1;
+`;
+
+const RecordForm = () => {
   const addRecord = useStore(state => state.addRecord);
   const updateTimeBank = useStore(state => state.updateTimeBank);
 
-  const [errors, setErrors] = useState({});
+  const [error, setError] = useState();
 
   const [punchIn, setPunchIn] = useState();
   const [punchOut, setPunchOut] = useState();
@@ -39,41 +105,22 @@ const Form = () => {
   };
 
   function validate() {
-    const requiredFieldMessage = 'Required field';
-    let isFormValid = true;
-    setErrors({});
+    setError('');
 
-    if (!punchIn) {
-      isFormValid = false;
-      setErrors(old => ({ ...old, punchIn: requiredFieldMessage }));
+    if (!punchIn || !punchOut || !breakDuration) {
+      setError('All fields are required');
+      return false;
     }
-    if (!punchOut) {
-      isFormValid = false;
-      setErrors(old => ({ ...old, punchOut: requiredFieldMessage }));
+    if (punchIn > punchOut) {
+      setError('Punch Out cannot be greater than Punch In');
+      return false;
     }
-    if (!breakDuration) {
-      isFormValid = false;
-      setErrors(old => ({ ...old, breakDuration: requiredFieldMessage }));
-    }
-    if (punchIn && punchOut) {
-      if (punchIn > punchOut) {
-        isFormValid = false;
-        setErrors(old => ({
-          ...old,
-          punchIn: 'End must be greater than start',
-          punchOut: 'End must be greater than start',
-        }));
-      }
-      if (timePassed() < breakDuration) {
-        isFormValid = false;
-        setErrors(old => ({
-          ...old,
-          breakDuration: 'Break cannot be greater than the total time passed',
-        }));
-      }
+    if (timePassed() < breakDuration) {
+      setError('Break cannot be greater than the total time passed');
+      return false;
     }
 
-    return isFormValid;
+    return true;
   }
 
   function clearInputs() {
@@ -99,49 +146,49 @@ const Form = () => {
         addRecord(res.data);
         updateTimeBank(calculateTimeBank(res.data));
         clearInputs();
-      } catch (er) {
-        console.log(er);
+      } catch (e) {
+        console.log(e);
       }
   };
 
   return (
-    <StyledForm onSubmit={e => createNewRecord(e)}>
-      <StyledFormGroup>
-        <StyledLabel>Punch In</StyledLabel>
-        <StyledInput
-          type="time"
-          id="punchIn"
-          value={punchIn}
-          onChange={e => setPunchIn(e.target.value)}
-          error
-        />
-        <Message>{errors.punchIn}</Message>
-      </StyledFormGroup>
-      <StyledFormGroup>
-        <StyledLabel>Punch Out</StyledLabel>
-        <StyledInput
-          type="time"
-          id="punchOut"
-          value={punchOut}
-          onChange={e => setPunchOut(e.target.value)}
-        />
-        <Message>{errors.punchOut}</Message>
-      </StyledFormGroup>
-      <StyledFormGroup>
-        <StyledLabel>Break Duration</StyledLabel>
-        <StyledInput
-          tye="number"
-          id="breakDuration"
-          value={breakDuration}
-          onChange={e => setBreakDuration(e.target.value)}
-        />
-        <Message>{errors.breakDuration}</Message>
-      </StyledFormGroup>
-      <StyledButton type="submit">
-        <GrDocumentTime />
-      </StyledButton>
-    </StyledForm>
+    <Card flexDirection="column">
+      <Form onSubmit={e => createNewRecord(e)}>
+        <FormGroup>
+          <Label htmlFor="punchIn">Punch In</Label>
+          <Input
+            type="time"
+            id="punchIn"
+            value={punchIn}
+            onChange={e => setPunchIn(e.target.value)}
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label htmlFor="punchIn">Punch Out</Label>
+          <Input
+            type="time"
+            id="punchOut"
+            value={punchOut}
+            onChange={e => setPunchOut(e.target.value)}
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label htmlFor="punchIn">Break</Label>
+          <Input
+            type="number"
+            id="breakDuration"
+            value={breakDuration}
+            onChange={e => setBreakDuration(e.target.value)}
+            min={1}
+          />
+        </FormGroup>
+        <Button type="submit">
+          <GrDocumentTime />
+        </Button>
+      </Form>
+      <ErrorMessage>{error}</ErrorMessage>
+    </Card>
   );
 };
 
-export default Form;
+export default RecordForm;
